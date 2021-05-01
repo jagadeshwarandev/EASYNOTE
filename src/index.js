735b39badee55650e72f4cdd5905b3a1dc15fb58
jagadeshwaran <76894794+jagadeshwarandev@users.noreply.github.com>
2021-05-01 01:16:21 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,12 +7,7 @@ import 'firebase/firestore';
 
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCqWauHEdRm6DzFQVbeQgp4bDV-9TWXQ6I",
-  authDomain: "evernote-4e902.firebaseapp.com",
-  projectId: "evernote-4e902",
-  storageBucket: "evernote-4e902.appspot.com",
-  messagingSenderId: "1021505010376",
-  appId: "1:1021505010376:web:bacff594156fe71493c3fd"
+  //api key comes here ;
 });
 
 ReactDOM.render(<App />, document.getElementById('root'));
