bfe26218ed11d1e892643e2074554bb5685694f8
jagadeshwaran <76894794+jagadeshwarandev@users.noreply.github.com>
2021-05-01 13:42:31 -0700

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+firebase.initializeApp({
+  apiKey: "AIzaSyCqWauHEdRm6DzFQVbeQgp4bDV-9TWXQ6I",
+  authDomain: "evernote-4e902.firebaseapp.com",
+  projectId: "evernote-4e902",
+  storageBucket: "evernote-4e902.appspot.com",
+  messagingSenderId: "1021505010376",
+  appId: "1:1021505010376:web:bacff594156fe71493c3fd"
+});
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
